6ff55f00e961f96880c2a2c4032628f572b9dd4b
Kaelin Lind <lind@esat-alumni.com>
2025-03-16 14:27:08 +0100

    Implemented JoyStick

--- Assets/Scripts/Player/player_movement.cs
+++ Assets/Scripts/Player/player_movement.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
+
+    JoyStick joyStick;
+
+    Vector3 joystick_dir;
+    Vector3 dir = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
+        joyStick = GameObject.FindGameObjectWithTag("JoyStick").GetComponent<JoyStick>();
         
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if(Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            touchPosition.z = 0.0f;
-            transform.position = touchPosition;
-        }
+    { 
+        joystick_dir = joyStick.dirVec;
+        dir = joystick_dir.normalized;
+        Debug.DrawLine(dir, transform.position, Color.magenta);
     }
 }
---
+++ Assets/Scripts/UI/JoyStick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStick : MonoBehaviour
+{
+    Vector3 originPos = Vector3.zero;
+    public SpriteRenderer sprite;
+    public Vector3 dirVec = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.touchCount > 0)
+        {
+            sprite.enabled = true;
+            Touch touch = Input.GetTouch(0);
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+            touchPos.z = 0.0f;
+            float distance = Vector3.Distance(touchPos, originPos);
+            if (distance < 1.5f)
+            {
+                transform.position = touchPos;
+                dirVec = touchPos - originPos;
+            }
+            if(touch.phase == TouchPhase.Began)
+            {
+                originPos = transform.position;
+            }
+            
+            Debug.DrawLine(originPos, transform.position, Color.red);
+        }
+        else
+        {
+            dirVec = Vector3.zero;
+            sprite.enabled = false;
+        }
+
+    }
+}
